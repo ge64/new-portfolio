008118e8c3910365a7fb22a14c25b0042d60090c
Gabriel Eaton <50244998+ge64@users.noreply.github.com>
2024-04-03 22:17:00 -0400

    starting with the grid

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -2,14 +2,28 @@
 import React from 'react';
 import { Container } from '@mui/material';
 import { Box } from '@mui/material';
+import { experimentalStyled as styled } from '@mui/material/styles';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Unstable_Grid2';
+
+const Item = styled(Paper)(({ theme }) => ({
+	backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+	...theme.typography.body2,
+	padding: theme.spacing(2),
+	textAlign: 'center',
+	color: theme.palette.text.secondary,
+}));
 
 const Intro = () => (
-	<Container sx={{ mt: 10, height: '100vg' }}>
+	<Container sx={{ mt: 10, height: '100vg', flexGrow: 1 }}>
 		<Box sx={{ height: 'calc(100vh - 64px)' }}>
-			<div className="content">
-				<h1>Welcome to My Portfolio</h1>
-				<p>This is an introduction to who I am and what I do.</p>
-			</div>
+			<Grid container spacing={{ xs: 1, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+				{Array.from(Array(6)).map((_, index) => (
+					<Grid xs={1} sm={4} md={4} key={index}>
+						<Item>xs=2</Item>
+					</Grid>
+				))}
+			</Grid>
 		</Box>
 	</Container>
 );
